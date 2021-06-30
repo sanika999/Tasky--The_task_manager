73e96bd22b43308390af3cc6eb0a1c5a51a71955
Sanika Dalvi <sanika.dalvi2000@gmail.com>
2021-06-30 10:01:17 +0530

    add contenteditable attribute dynamically

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ const newCard = ({
 }) => `<div class="col-md-6 col-lg-4" id=${id}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success">
-      <i class="fas fa-pencil-alt"></i>
+    <button type="button" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)">
+      <i class="fas fa-pencil-alt" id=${id}></i>
     </button>
     <button type="button" id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
-      <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
+      <i class="fas fa-trash" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
     </button>
   </div>
   <img
     src=${imageUrl}
     class="card-img-top"
-    alt="..."
+    alt="image"
   />
   <div class="card-body">
     <h5 class="card-title">${taskTitle}</h5>
@@ -102,4 +102,28 @@ const deleteCard = (event) => {
   return taskContainer.removeChild(
     event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4
   );
+}; 
+
+//contenteditable
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+  let parentElement ;
+
+  if (tagname === "BUTTON"){
+    parentElement = event.target.parentNode.parentNode;
+  } else {
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+  submitButton.innerHTML = "Save Changes"
 };
